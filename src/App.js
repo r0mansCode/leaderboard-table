1d313e74365a7e4b8c56804bbf17e186778d78ab
r0mansCode <kovaljonokroman@gmail.com>
2022-09-07 21:28:34 +0300

    setting up the background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+// import Character_left from "./assets/images/"
+// import Character_Right from "./assets/images/Character_Right.svg"
+// import Character_Left from "./assets/images/Character_Left.svg"
 
 function App() {
   return (
-    <div className="App">
-      HALL OF FAME
+    <div className="AppContainer">
+      Hello
     </div>
   );
 }
