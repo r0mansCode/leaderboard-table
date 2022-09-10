11d3a1a334a42ff0b3393e7bf4a75249e76396a6
r0mansCode <kovaljonokroman@gmail.com>
2022-09-10 11:18:33 +0300

    adding table badges

--- src/App.js
+++ src/App.js
@@ -1,57 +1,54 @@
-import './App.css';
-import first_place from "./assets/images/first_place.svg"
-import second_place from "./assets/images/second_place.svg"
-import third_place from "./assets/images/third_place.svg"
-import { Table } from "./table/Table"
+import "./App.css";
+import first_place from "./assets/images/first_place.svg";
+import second_place from "./assets/images/second_place.svg";
+import third_place from "./assets/images/third_place.svg";
+import { Table } from "./table/Table";
 
 function App() {
   return (
     <div className="AppContainer">
-      <div className='title'>HALL OF FAME</div>
-      <div className='description'>
+      <div className="title">HALL OF FAME</div>
+      <div className="description">
         Have you ever dreamed of taking part in a tournament? <br />
-        Galloping on a horse while holding a spear and trying to knock<br /> your enemy out of…
+        Galloping on a horse while holding a spear and trying to knock
+        <br /> your enemy out of…
       </div>
-      <div className='button'>
-        Join now
-      </div>
-      <div className='winnerContainer'>
-        <div className='Place_second'>
-          <img src={second_place}/>
-          <div className='winContainer winContainer--second'>
-            <div className='nickname nickname--second'>AP184***</div>
+      <div className="button">Join now</div>
+      <div className="winnerContainer">
+        <div className="Place_second">
+          <img src={second_place} />
+          <div className="winContainer winContainer--second">
+            <div className="nickname nickname--second">AP184***</div>
             <div>BET</div>
-            <div className='secondFont'>€61</div>
-            <div className='yellow yellow--small'>WIN</div>
-            <div className='yellow'>€1852</div>
+            <div className="secondFont">€61</div>
+            <div className="yellow yellow--small">WIN</div>
+            <div className="yellow">€1852</div>
           </div>
         </div>
-        <div className='Place_first'>
+        <div className="Place_first">
           <img src={first_place} />
-          <div className='winContainer winContainer--first'>
-          <div className='nickname nickname--first'>Kate***</div>
+          <div className="winContainer winContainer--first">
+            <div className="nickname nickname--first">Kate***</div>
             <div>BET</div>
-            <div className='secondFont'>€461</div>
-            <div className='yellow yellow--small'>WIN</div>
-            <div className='yellow'>€61852</div>
+            <div className="secondFont">€461</div>
+            <div className="yellow yellow--small">WIN</div>
+            <div className="yellow">€61852</div>
           </div>
         </div>
-        <div className='Place_third'>
+        <div className="Place_third">
           <img src={third_place} />
-          <div className='winContainer winContainer--third'>
-          <div className='nickname nickname--third'>Lig***</div>
+          <div className="winContainer winContainer--third">
+            <div className="nickname nickname--third">Lig***</div>
             <div>BET</div>
-            <div className='secondFont'>€12</div>
-            <div className='yellow yellow--first'>WIN</div>
-            <div className='yellow'>1000000 HUF</div>
+            <div className="secondFont">€12</div>
+            <div className="yellow yellow--first">WIN</div>
+            <div className="yellow">1000000 HUF</div>
           </div>
         </div>
       </div>
-      <div className='title title--table'>TOP WINNERS</div>
+      <div className="title title--table">TOP WINNERS</div>
       <Table />
-      <div className='button button--showResults'>
-        Show all results
-      </div>
+      <div className="button button--showResults">Show all results</div>
     </div>
   );
 }
--- src/table/Table.js
+++ src/table/Table.js
@@ -1,31 +1,43 @@
-import './Table.css';
-import table_fire from "../assets/images/table_fire.svg"
-import data from "../data/data.json"
+import "./Table.css";
+import table_fire from "../assets/images/table_fire.svg";
+import data from "../data/data.json";
 
 export const Table = () => {
-    return (
-        <div className='tableContainer'>
-            <img src={table_fire} className="tableFire" />
-        <table>
-            <thead>
-                <tr>
-                    <th>Place</th>
-                    <th>Player</th>
-                    <th>Score</th>
-                    <th>Prize</th>
-                </tr>
-            </thead>
+  return (
+    <div className="tableContainer">
+      <img src={table_fire} className="tableFire" />
+      <table>
+        <thead>
+          <tr>
+            <th>Place</th>
+            <th>Player</th>
+            <th>Score</th>
+            <th>Prize</th>
+          </tr>
+        </thead>
         <tbody>
-        {data.map((row, ind) => (
+          {data.map((row, ind) => (
             <tr key={ind}>
-              <td>{row.place}</td>
+              <td
+                className={
+                  row.place === 1
+                    ? "badge badge--first"
+                    : row.place === 2
+                    ? "badge badge--second"
+                    : row.place === 3
+                    ? "badge badge--third"
+                    : null
+                }
+              >
+                {row.place}
+              </td>
               <td>{row.player}</td>
               <td>{row.score}</td>
               <td>{row.prize}</td>
             </tr>
           ))}
         </tbody>
-        </table>
-        </div>
-    )
-}
+      </table>
+    </div>
+  );
+};
